893949a4a92268e82b408762d1a640f7ad8aad0b
matajeric <matajeric2006@gmail.com>
2024-05-20 17:58:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.recyclerviewtest;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    Adapter adapter;
+    ArrayList<String> items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        items = new ArrayList<>();
+        items.add("Number 2");
+        items.add("Number 2");
+        items.add("Number 3");
+        items.add("Number 4");
+        items.add("Number 5");
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adapter(this,items);
+        recyclerView.setAdapter(adapter);
+
+    }
+}
